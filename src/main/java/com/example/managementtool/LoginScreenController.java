11dd2cb94874d781cc2732ea959c1d2dfbed46de
Kyle <kylematinzi@yahoo.com>
2022-10-23 15:58:55 -0700

    Update

--- src/main/java/com/example/managementtool/LoginScreenController.java
+++ src/main/java/com/example/managementtool/LoginScreenController.java
@@ -1,4 +1,47 @@
 package com.example.managementtool;
 
-public class LoginScreenController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginScreenController implements Initializable {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button closeButton;
+
+    public void loginButtonPressed (ActionEvent actionEvent) throws IOException {
+        StackPane adminDashboardParent = new StackPane();
+        adminDashboardParent.getChildren().add(FXMLLoader.load(getClass().getResource("AdminDashboard.fxml")));
+        Scene scene = new Scene(adminDashboardParent);
+        Stage adminDashboardScene = new Stage();
+        adminDashboardScene.setScene(scene);
+        adminDashboardScene.initModality(Modality.WINDOW_MODAL);
+        adminDashboardScene.initOwner(((((Button)actionEvent.getSource()).getScene().getWindow())));
+        adminDashboardScene.sizeToScene();
+        adminDashboardScene.setResizable(false);
+        adminDashboardScene.setTitle("Project Management System");
+        adminDashboardScene.show();
+    }
+
+    public void closeButtonPressed(ActionEvent actionEvent) throws IOException {
+        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/com/example/managementtool/UserDashboardController.java
+++ src/main/java/com/example/managementtool/UserDashboardController.java
@@ -1,4 +1,26 @@
 package com.example.managementtool;
 
-public class UserDashboardController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserDashboardController implements Initializable {
+
+    @FXML
+    private Button closeButton;
+
+    public void closeButtonPressed(ActionEvent actionEvent) throws IOException {
+        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
