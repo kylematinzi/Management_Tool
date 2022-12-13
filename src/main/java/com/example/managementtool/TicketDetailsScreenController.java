f2ae6152a1424170e0ed58603d4601bfc8309ab7
Kyle <kylematinzi@yahoo.com>
2022-12-13 07:26:12 -0800

    Projects view updates.

--- src/main/java/com/example/managementtool/TicketDetailsScreenController.java
+++ src/main/java/com/example/managementtool/TicketDetailsScreenController.java
@@ -12,14 +12,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
 
 public class TicketDetailsScreenController implements Initializable {
 
-    private Ticket selectedTicket;
     @FXML
     private TextField ticketTitleTextField;
     @FXML
@@ -27,22 +24,15 @@ public class TicketDetailsScreenController implements Initializable {
     @FXML
     private TextArea ticketDescriptionTextArea;
     @FXML
-    private ComboBox ticketStatusComboBox;
+    private ComboBox<String> ticketStatusComboBox;
     @FXML
-    private ComboBox ticketPriorityComboBox;
+    private ComboBox<String> ticketPriorityComboBox;
     @FXML
-    private ComboBox projectIdComboBox;
+    private ComboBox<Integer> projectIdComboBox;
     @FXML
     private Label dateCreatedLabel;
-    private String ticketTitle;
-    private int ticketId;
-    private int projectAssociation;
-    private String ticketStatus;
-    private String ticketPriority;
-    private String ticketDescription;
 
     public void getInitializeData(Ticket ticket){
-        selectedTicket = ticket;
         ticketTitleTextField.setText(ticket.getTicketTitle());
         projectIdComboBox.setValue(ticket.getProjectAssociation());
         ticketIdTextField.setText(String.valueOf(ticket.getTicketId()));
@@ -55,18 +45,18 @@ public class TicketDetailsScreenController implements Initializable {
     }
 
     // TODO create function that if user selects ticket complete as status date completed is set.
-    public void saveChangesButtonPressed(ActionEvent actionEvent) throws SQLException {
+    public void saveChangesButtonPressed(ActionEvent actionEvent) {
         try {
             Connection conn = DatabaseConnection.getConnection();
             String updateStatement = "UPDATE Ticket_Table SET Ticket_Title = ?, Project_Association = ?, Priority_Level = ?, " +
                     " Ticket_Status = ?, Ticket_Description = ? WHERE Ticket_Id = ?";
             DatabaseQuery.setPreparedStatement(conn, updateStatement);
-            ticketTitle = ticketTitleTextField.getText();
-            projectAssociation = (int) projectIdComboBox.getValue();
-            ticketPriority = String.valueOf(ticketPriorityComboBox.getValue());
-            ticketStatus = String.valueOf(ticketStatusComboBox.getValue());
-            ticketDescription = ticketDescriptionTextArea.getText();
-            ticketId = Integer.parseInt(ticketIdTextField.getText());
+            String ticketTitle = ticketTitleTextField.getText();
+            int projectAssociation = projectIdComboBox.getValue();
+            String ticketPriority = String.valueOf(ticketPriorityComboBox.getValue());
+            String ticketStatus = String.valueOf(ticketStatusComboBox.getValue());
+            String ticketDescription = ticketDescriptionTextArea.getText();
+            int ticketId = Integer.parseInt(ticketIdTextField.getText());
             PreparedStatement ps = DatabaseQuery.getPreparedStatement();
             ps.setString(1, ticketTitle);
             ps.setInt(2, projectAssociation);
@@ -77,18 +67,18 @@ public class TicketDetailsScreenController implements Initializable {
             ps.execute();
             Alert alert = new Alert(Alert.AlertType.INFORMATION, "Ticket Id: " + ticketId + ": " + ticketTitle + " has been updated.");
             alert.setTitle("Confirmation");
-            Optional<ButtonType> result = alert.showAndWait();
+            //Optional<ButtonType> result = alert.showAndWait();
             ((Stage) (((Button) actionEvent.getSource()).getScene().getWindow())).close();
             AdminDashboardController tableRefresh = new AdminDashboardController();
             tableRefresh.refreshAdminTicketTable();
-        }catch (Exception e){
+        }catch (Exception ignored){
 
         }
 
     }
 
     public void removeTeamMemberButtonPressed(ActionEvent actionEvent){
-
+        System.out.println("remove team pressed");
     }
     public void closeButtonPressed(ActionEvent actionEvent) throws IOException {
         ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
--- src/main/java/com/example/managementtool/allProjectsScreenController.java
+++ src/main/java/com/example/managementtool/allProjectsScreenController.java
@@ -1,9 +1,14 @@
 package com.example.managementtool;
 
+import Utility.DatabaseAccess;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,13 +19,36 @@ public class allProjectsScreenController implements Initializable {
 
     @FXML
     private Button closeButton;
+    @FXML
+    private ListView<String> projectsListView;
+    @FXML
+    private PieChart projectsPieChart;
 
+    private static final ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+
+    public static ObservableList<PieChart.Data> fillProjectsPieChart(){
+        // just test data
+        pieChartData.clear();
+        pieChartData.add(new PieChart.Data("Not started", 20));
+        pieChartData.add(new PieChart.Data("In work", 50));
+        pieChartData.add(new PieChart.Data("Complete", 20));
+        pieChartData.add(new PieChart.Data("Unassigned", 10));
+        System.out.println(pieChartData);
+        return pieChartData;
+    }
     public void closeButtonPressed(ActionEvent actionEvent) throws IOException {
         ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            projectsListView.setItems(DatabaseAccess.getAllProjectNames());
+            projectsPieChart.setData(fillProjectsPieChart());
+        }catch (Exception e){
+
+        }
 
     }
 }
