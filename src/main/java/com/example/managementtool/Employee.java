734be728b29afd5fb7715c67b6e8fdc9327d7d16
Kyle <kylematinzi@yahoo.com>
2023-01-13 06:41:50 -0800

    Cleanup and comments.

--- src/main/java/com/example/managementtool/Employee.java
+++ src/main/java/com/example/managementtool/Employee.java
@@ -1,21 +1,28 @@
 package com.example.managementtool;
 
+/**
+ * This class allows the creation of the employee object
+ */
 public class Employee {
 
     private int employeeId;
-
     private String employeeFirstName;
-
     private String employeeLastName;
-
     private String userName;
-
     private String jobTitle;
-
     private String emailAddress;
-
     private String employeePassword;
 
+    /**
+     * This method is the constructor for the employee object.
+     * @param employeeId - employee's user id
+     * @param employeeFirstName - employee's first name
+     * @param employeeLastName - employee's last name
+     * @param userName - employee's username
+     * @param jobTitle - employee's job title
+     * @param emailAddress - employee's email address
+     * @param employeePassword - employee's password
+     */
     public Employee(int employeeId, String employeeFirstName, String employeeLastName, String userName, String jobTitle,
                     String emailAddress, String employeePassword){
         this.employeeId = employeeId;
@@ -28,6 +35,8 @@ public class Employee {
 
     }
 
+    // Below are the getters and setters for the employee class. I'm not going to comment each one as I find it pretty self-
+    // evident on what each one does.
     public int getEmployeeId() {
         return employeeId;
     }
