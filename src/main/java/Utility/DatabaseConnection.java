af58d98a5666d6f475b07f02b3e75654c0d23fec
Kyle <kylematinzi@yahoo.com>
2023-01-15 14:50:06 -0800

    Cleanup and comments.

--- src/main/java/Utility/DatabaseConnection.java
+++ src/main/java/Utility/DatabaseConnection.java
@@ -1,38 +1,45 @@
 package Utility;
 import java.sql.*;
 
-
+/**
+ * This class is used to start and close the connection with the mysql database.
+ */
 public class DatabaseConnection {
-
     private static Connection connection = null;
 
-
+    /**
+     * This method starts the connection with the database.
+     * @return - connection
+     */
     public static Connection startConnection(){
         try{
             connection = DriverManager.getConnection("jdbc:mysql://management-tool-database.c9wsdjztcano.us-east-1.rds.amazonaws.com:3306/Management_Tool_Database","admin", "AdmiN305");
             Statement statement = (connection).createStatement();
             ResultSet resultSet = statement.executeQuery("Select * from Employee_Table");
-
             while (resultSet.next()){
                 System.out.println(resultSet.getString("Employee_Last_Name"));
             }
-
         } catch (Exception e){
             e.printStackTrace();
         }
-
         return connection;
     }
 
+    /**
+     * This method is used to close an open connection with the database.
+     */
     public static void closeConnection(){
         try{
             connection.close();
         }
         catch (Exception e){
-
         }
     }
 
+    /**
+     * This method is used to retrieve the open database connection.
+     * @return - connection
+     */
     public static Connection getConnection(){
         return connection;
     }
--- src/main/java/Utility/DatabaseQuery.java
+++ src/main/java/Utility/DatabaseQuery.java
@@ -4,16 +4,29 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * This class is used for sending queries to the database.
+ */
 public class DatabaseQuery {
 
     private static PreparedStatement preparedStatement;
 
+    /**
+     * This method will set the prepared statement for querying the database.
+     * @param conn - connection to the database
+     * @param sqlStatement - statement being sent to the database
+     * @throws SQLException
+     */
     public static void setPreparedStatement(Connection conn, String sqlStatement) throws SQLException {
 
         preparedStatement = conn.prepareStatement(sqlStatement);
 
     }
 
+    /**
+     * This method retrieves the prepared statement.
+     * @return - prepared statement
+     */
     public static PreparedStatement getPreparedStatement(){
         return preparedStatement;
     }
