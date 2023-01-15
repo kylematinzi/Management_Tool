078f6aa93224906b202c6fda234985e2c9469281
Kyle <kylematinzi@yahoo.com>
2023-01-15 13:30:20 -0800

    Cleanup and comments.

--- src/main/java/com/example/managementtool/TicketDetailsScreenController.java
+++ src/main/java/com/example/managementtool/TicketDetailsScreenController.java
@@ -12,9 +12,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
-
+/**
+ * This class is the ticket details screen controller. This screen displays to the user a selected tickets details and allows
+ * them to edit the ticket.
+ */
 public class TicketDetailsScreenController implements Initializable {
 
     @FXML
@@ -32,6 +36,10 @@ public class TicketDetailsScreenController implements Initializable {
     @FXML
     private Label dateCreatedLabel;
 
+    /**
+     * This method is used to retrieve the data to initialize the ticket detail screen with the correct default data.
+     * @param ticket - selected ticket
+     */
     public void getInitializeData(Ticket ticket){
         ticketTitleTextField.setText(ticket.getTicketTitle());
         projectIdComboBox.setValue(ticket.getProjectAssociation());
@@ -40,10 +48,13 @@ public class TicketDetailsScreenController implements Initializable {
         dateCreatedLabel.setText(ticket.getDateCreated().toString());
         ticketStatusComboBox.setValue(ticket.getTicketStatusLevel());
         ticketPriorityComboBox.setValue(ticket.getTicketPriorityLevel());
-
-
     }
 
+    /**
+     * This method is used to update the ticket details when the save changes button is pressed. When pressed the method
+     * sends an update query to the database updating any fields the user updated provided the ticket detail screen.
+     * @param actionEvent
+     */
     // TODO create function that if user selects ticket complete as status date completed is set.
     public void saveChangesButtonPressed(ActionEvent actionEvent) {
         try {
@@ -67,23 +78,37 @@ public class TicketDetailsScreenController implements Initializable {
             ps.execute();
             Alert alert = new Alert(Alert.AlertType.INFORMATION, "Ticket Id: " + ticketId + ": " + ticketTitle + " has been updated.");
             alert.setTitle("Confirmation");
-            //Optional<ButtonType> result = alert.showAndWait();
+            Optional<ButtonType> result = alert.showAndWait();
             ((Stage) (((Button) actionEvent.getSource()).getScene().getWindow())).close();
             AdminDashboardController tableRefresh = new AdminDashboardController();
             tableRefresh.refreshAdminTicketTable();
         }catch (Exception ignored){
-
         }
-
     }
 
+    /**
+     * This method removes a team member from the selected ticket when the remove team member button is pressed.
+     * @param actionEvent
+     */
+    //TODO create method
     public void removeTeamMemberButtonPressed(ActionEvent actionEvent){
         System.out.println("remove team pressed");
     }
+
+    /**
+     * This method closes the ticket details screen when the close button is pressed.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void closeButtonPressed(ActionEvent actionEvent) throws IOException {
         ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
     }
 
+    /**
+     * This method initializes the ticket details screen with the correct data when the ticket details screen is loaded.
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ticketStatusComboBox.setItems(DatabaseAccess.getTicketStatusTypes());
